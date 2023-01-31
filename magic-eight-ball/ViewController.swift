2f0d029dcf30a2ee5676d9fb59fea292754f4eeb
Omaridavis2 <111915330+Omaridavis2@users.noreply.github.com>
2023-01-30 16:54:02 -0800

    Add files via upload

---
+++ magic-eight-ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-8-Ball2
+//
+//  Created by Omari Davis on 1/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
